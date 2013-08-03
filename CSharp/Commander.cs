1ea3b521386e2b6e237de7d7e511a2eef5a420e0
slick06551 <Brian.Slick@GENHQ024.geneca.com>
2013-08-03 15:23:43 -0500

    Test

--- CSharp/Commander.cs
+++ CSharp/Commander.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CruiseControl.Enums;
 
 namespace CruiseControl
 {
     public class Commander
     {
         public BoardStatus _currentBoard;
+        private const double MIN_HEALTH_BEFORE_REPAIR = .25;
 
         public Commander()
         {
@@ -19,11 +21,56 @@ namespace CruiseControl
         public List<Command> GiveCommands()
         {
             var cmds = new List<Command>();
+            bool moveShipFromWall = false;
+            
+            //Check board status to see if we need to move any ships
+            int teamCounter = 0;
+            foreach(var team in _currentBoard.VesselCountPerTeamId)
+            {
+                if (team.VesselCount > 0)
+                    teamCounter++;
+            }
+
+            if (_currentBoard.TurnsUntilBoardShrink <= teamCounter)
+                moveShipFromWall = true;
+
+            //Loop through each vessel to see if we can issue a command
+            foreach (var vessel in _currentBoard.MyVesselStatuses)
+            {
+                if (vessel.Health > 0 && vessel.MovesUntilRepair==0)
+                {
+                    if (moveShipFromWall)
+                    {
+                        var direction = GetDirectionToMoveVesselFromEdge(vessel.Location, _currentBoard.BoardMinCoordinate, _currentBoard.BoardMaxCoordinate);
+                        if (direction != String.Empty)
+                        {
+                            cmds.Add(new Command { vesselid = vessel.Id, action = String.Format(CommandType.Move, direction) });
+                            continue;
+                        }
+                    }
+
+                    //Check if we should move to collide with ship
+
+                    var coordinateToFireMissile = GetCoordinateToFireMissile(vessel.SonarReport);
+                    if(coordinateToFireMissile != null)
+                        cmds.Add(new Command { vesselid = vessel.Id, action = CommandType.Fire
+                            ,coordinate = new Coordinate { X = coordinateToFireMissile.X, Y = coordinateToFireMissile.Y }
+                        });
+                        
+                    if (vessel.Health < (MIN_HEALTH_BEFORE_REPAIR * vessel.MaxHealth))
+                    {
+                        cmds.Add(new Command { vesselid = vessel.Id, action = CommandType.Repair });
+                        continue;
+                    }
+
+                    //Check to move
+                }
+            }
 
             // Add Commands Here.
             // You can only give as many commands as you have un-sunk vessels. Powerup commands do not count against this number. 
             // You are free to use as many powerup commands at any time. Any additional commands you give (past the number of active vessels) will be ignored.
-			cmds.Add(new Command { vesselid = 1, action = "fire", coordinate = new Coordinate { X = 1, Y = 1 } });
+			cmds.Add(new Command { vesselid = 1, action = CommandType.Fire, coordinate = new Coordinate { X = 1, Y = 1 } });
 
             return cmds;
         }
@@ -39,5 +86,42 @@ namespace CruiseControl
         {
 
         }
+
+        private static string GetDirectionToMoveVesselFromEdge(List<Coordinate> vesselCoords, Coordinate boardMin, Coordinate boardMax)
+        {
+            foreach (var coord in vesselCoords)
+            {
+                if (coord.X == boardMin.X)
+                    return "east";
+                if(coord.X == boardMax.X)
+                    return "west";
+                if (coord.Y > boardMin.Y)
+                    return "south";
+                if(coord.Y == boardMax.Y)
+                    return "north";
+            }
+            return String.Empty;
+        }
+
+        private static Coordinate GetDirectionToMoveVesselIntoCollision(List<Coordinate> sonarReport)
+        {
+            if (sonarReport.Count > 0)
+            { 
+                return sonarReport[sonarReport.Count/2];
+            }
+
+            return null;
+        }
+
+        private static Coordinate GetCoordinateToFireMissile(List<Coordinate> sonarReport)
+        {
+            if (sonarReport.Count > 0)
+            {
+                return sonarReport[sonarReport.Count / 2];
+            }
+
+            return null;
+        }
+
     }
 }
---
+++ CSharp/Enums/CommandType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CruiseControl.Enums
+{
+    public static class CommandType
+    {
+        public const string Fire = "fire";
+        public const string Move = "move:{0}";
+        public const string LoadCountermeasures = "load_countermeasures";
+        public const string Repair = "repair";
+        public const string Truce = "truce:{0}";
+        public const string AcceptTruce = "accept_truce:{0}";
+    }
+}

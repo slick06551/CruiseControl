983f9f911fec1a74cdaf99c581f713f60d22b31a
slick06551 <Brian.Slick@GENHQ024.geneca.com>
2013-08-03 15:50:26 -0500

    3

--- CSharp/Commander.cs
+++ CSharp/Commander.cs
@@ -21,56 +21,64 @@ namespace CruiseControl
         public List<Command> GiveCommands()
         {
             var cmds = new List<Command>();
-            //bool moveShipFromWall = false;
+            bool moveShipFromWall = false;
+
+            //Check board status to see if we need to move any ships
+            int teamCounter = 0;
+            foreach (var team in _currentBoard.VesselCountPerTeamId)
+            {
+                if (team.VesselCount > 0)
+                    teamCounter++;
+            }
+
+            if (_currentBoard.TurnsUntilBoardShrink <= teamCounter)
+                moveShipFromWall = true;
+
+            //Loop through each vessel to see if we can issue a command
+            foreach (var vessel in _currentBoard.MyVesselStatuses)
+            {
+                if (vessel.Health > 0 && vessel.MovesUntilRepair == 0)
+                {
+                    if (moveShipFromWall)
+                    {
+                        var direction = GetDirectionToMoveVesselFromEdge(vessel.Location, _currentBoard.BoardMinCoordinate, _currentBoard.BoardMaxCoordinate);
+                        if (direction != String.Empty)
+                        {
+                            cmds.Add(new Command { vesselid = vessel.Id, action = String.Format(CommandType.Move, direction) });
+                            continue;
+                        }
+                    }
+
+                    //Check if we should move to collide with ship
+                    var directionForCollision = GetDirectionToMoveVesselIntoCollision(vessel.Location, vessel.SonarReport);
+                    if (directionForCollision != null)
+                        cmds.Add(new Command
+                        {
+                            vesselid = vessel.Id,
+                            action = String.Format(CommandType.Move,directionForCollision)
+                        });
+
+                    var coordinateToFireMissile = GetCoordinateToFireMissile(vessel.SonarReport);
+                    if (coordinateToFireMissile != null)
+                        cmds.Add(new Command
+                        {
+                            vesselid = vessel.Id,
+                            action = CommandType.Fire
+                            ,
+                            coordinate = new Coordinate { X = coordinateToFireMissile.X, Y = coordinateToFireMissile.Y }
+                        });
+
+                    if (vessel.Health < (MIN_HEALTH_BEFORE_REPAIR * vessel.MaxHealth))
+                    {
+                        cmds.Add(new Command { vesselid = vessel.Id, action = CommandType.Repair });
+                        continue;
+                    }
+
+                    //Check to move
+                }
+            }
+
             
-            ////Check board status to see if we need to move any ships
-            //int teamCounter = 0;
-            //foreach(var team in _currentBoard.VesselCountPerTeamId)
-            //{
-            //    if (team.VesselCount > 0)
-            //        teamCounter++;
-            //}
-
-            //if (_currentBoard.TurnsUntilBoardShrink <= teamCounter)
-            //    moveShipFromWall = true;
-
-            ////Loop through each vessel to see if we can issue a command
-            //foreach (var vessel in _currentBoard.MyVesselStatuses)
-            //{
-            //    if (vessel.Health > 0 && vessel.MovesUntilRepair==0)
-            //    {
-            //        if (moveShipFromWall)
-            //        {
-            //            var direction = GetDirectionToMoveVesselFromEdge(vessel.Location, _currentBoard.BoardMinCoordinate, _currentBoard.BoardMaxCoordinate);
-            //            if (direction != String.Empty)
-            //            {
-            //                cmds.Add(new Command { vesselid = vessel.Id, action = String.Format(CommandType.Move, direction) });
-            //                continue;
-            //            }
-            //        }
-
-            //        //Check if we should move to collide with ship
-
-            //        var coordinateToFireMissile = GetCoordinateToFireMissile(vessel.SonarReport);
-            //        if(coordinateToFireMissile != null)
-            //            cmds.Add(new Command { vesselid = vessel.Id, action = CommandType.Fire
-            //                ,coordinate = new Coordinate { X = coordinateToFireMissile.X, Y = coordinateToFireMissile.Y }
-            //            });
-                        
-            //        if (vessel.Health < (MIN_HEALTH_BEFORE_REPAIR * vessel.MaxHealth))
-            //        {
-            //            cmds.Add(new Command { vesselid = vessel.Id, action = CommandType.Repair });
-            //            continue;
-            //        }
-
-            //        //Check to move
-            //    }
-            //}
-
-            // Add Commands Here.
-            // You can only give as many commands as you have un-sunk vessels. Powerup commands do not count against this number. 
-            // You are free to use as many powerup commands at any time. Any additional commands you give (past the number of active vessels) will be ignored.
-			//cmds.Add(new Command { vesselid = 1, action = CommandType.Fire, coordinate = new Coordinate { X = 1, Y = 1 } });
 
             return cmds;
         }
